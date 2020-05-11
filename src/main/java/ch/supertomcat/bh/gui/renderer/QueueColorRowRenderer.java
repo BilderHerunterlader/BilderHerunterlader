65f26f23cb2bf93f87c8ad85916a909d296b5adc
supertomcat <bilderherunterlader@gmail.com>
2020-05-12 01:23:18 +0200

    Bug Fixed: When a Pic is disabled, but the states is failed, the foreground color of the progress column must not be changed to red, because the brackground is already red.

--- src/main/java/ch/supertomcat/bh/gui/renderer/QueueColorRowRenderer.java
+++ src/main/java/ch/supertomcat/bh/gui/renderer/QueueColorRowRenderer.java
@@ -2,11 +2,15 @@ package ch.supertomcat.bh.gui.renderer;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 
 import ch.supertomcat.bh.pic.Pic;
+import ch.supertomcat.bh.pic.PicState;
 import ch.supertomcat.bh.settings.SettingsManager;
 import ch.supertomcat.supertomcatutils.gui.table.renderer.DefaultStringColorRowRenderer;
 
@@ -15,6 +19,11 @@ import ch.supertomcat.supertomcatutils.gui.table.renderer.DefaultStringColorRowR
  */
 public class QueueColorRowRenderer extends DefaultStringColorRowRenderer implements TableCellRenderer {
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Failed Pic States
+	 */
+	private static final Set<PicState> FAILED_PIC_STATES = new HashSet<>(Arrays.asList(PicState.FAILED, PicState.FAILED_FILE_NOT_EXIST, PicState.FAILED_FILE_TEMPORARY_OFFLINE));
 
 	/**
 	 * Settings Manager
@@ -35,8 +44,9 @@ public class QueueColorRowRenderer extends DefaultStringColorRowRenderer impleme
 		if (isSelected) {
 			super.prepareForegroundColor(comp, table, value, isSelected, hasFocus, row, column);
 		} else {
+			boolean progressColumn = table.convertColumnIndexToModel(column) == 3;
 			Object progressValue = table.getModel().getValueAt(table.convertRowIndexToModel(row), 3);
-			if (progressValue instanceof Pic && ((Pic)progressValue).isDeactivated()) {
+			if (progressValue instanceof Pic && ((Pic)progressValue).isDeactivated() && (!progressColumn || !FAILED_PIC_STATES.contains(((Pic)progressValue).getStatus()))) {
 				comp.setForeground(Color.RED);
 			} else {
 				super.prepareForegroundColor(comp, table, value, isSelected, hasFocus, row, column);
