5346dde4e0b12ee69bef0986324c2a15fb3f9fcd
supertomcat <bilderherunterlader@gmail.com>
2020-07-31 22:09:59 +0200

    Added batch file to create update zip file

---
+++ createUpdateZipFile.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+::Version in Datei schreiben
+java -jar BH.jar -version > v.txt
+
+::Datei einlesen
+for /f "tokens=1" %%i in (v.txt) do set version=%%i
+::Dateie loeschen
+del v.txt
+
+::version einsetzen und zip datei erstellen
+"%programfiles%\7-zip\7z.exe" a -tzip BH-%version%.zip lib\*.jar -r
+"%programfiles%\7-zip\7z.exe" a -tzip BH-%version%.zip BH.jar
